7d985b30858f1978afe87aceef9264f18fbd1c46
GuiLangher <guilherme.langher@gmail.com>
2024-01-18 13:28:20 -0300

    trocar variaveis

---
+++ exercicio_trocarvariaveis.py
@@ -0,0 +1,20 @@
+
+"""
+
+Trocar variaveis
+
+"""
+
+x = input("Insira valor de x: ")
+y = input("Insira valor de y: ")
+
+#criando uma variavel temporaria
+
+temp = x
+x = y
+y = temp
+
+print(f"O valor de x depois da troca: {x}")
+print(f"O valor de y depois da troca: {y}")
+
+
