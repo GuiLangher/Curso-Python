17fa7868540d643eba2c4b8baa9f5361856217c0
Guilherme-Langher <guilherme.langher@gmail.com>
2024-01-25 21:26:35 -0300

    exerciciosGPT

---
+++ exercicios-GPT.py
@@ -0,0 +1,61 @@
+#Exercício 1: Soma de Dois Números
+"""
+print("Digite dois numeros para serem somados")
+
+num1 = int(input("Numero 1: "))
+num2 = int(input("Numero 2: "))
+
+Resultado = print(f"{num1} + {num2} = {num1 + num2}")
+
+"""
+#Exercício 2: Verificador de Números Pares
+"""
+print ("Digite um numero para saber se é par ou impar")
+
+numero = int(input("Digite o numero: "))
+
+if (numero % 2) == 0:
+    print(f"O numero {numero} é par")
+
+else:
+    print(f"O numero {numero} é impar")
+"""
+"""
+#Exercício 3: Calculadora de Média
+
+med1 = float(input("Digite a primeira nota: "))
+med2 = float(input("Digite a segunda nota: "))
+med3 = float(input("Digite a terceira nota: "))
+
+media = (med1 + med2 + med3) / 3
+
+print(media)
+"""
+"""
+#Exercício 4: Conversor de Temperatura
+#Crie um programa que converte a temperatura de Celsius para Fahrenheit. O usuário deve inserir a temperatura em Celsius.
+
+celsius = float(input("Digite a temperatura em celsius: "))
+
+fah = ()
+
+temp =(celsius * 1.8) + 32
+
+temperatura = print(f"A temperatura em Fahrenheit é: {temp}")
+"""
+
+#Exercício 5: Calculadora de Fatorial
+"""
+numero = int(input("insira um numero: "))
+
+fatorial = 1
+
+if numero <0:
+    print(f"O {numero} não é fatorail")
+    
+else:
+    for x in range(1, numero +1):
+        fatorial = fatorial*x
+    print(f"O fatorial de {numero} é: {fatorial}")
+"""
+
